b4a1c1bcac51e011800e0a00dd7c83d37a31e638
ngactive99 <ngactive99@gmail.com>
2022-03-21 14:43:23 +0530

    added rest template and both services are communicating with each other

--- user_service/src/main/java/com/user/controller/UserController.java
+++ user_service/src/main/java/com/user/controller/UserController.java
@@ -1,11 +1,12 @@
 package com.user.controller;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.client.RestTemplate;
 import com.user.entity.User;
 import com.user.service.UserService;
 
@@ -14,11 +15,18 @@ import com.user.service.UserService;
 public class UserController {
 	@Autowired
 	private UserService userService;
+	@Autowired
+	private RestTemplate restTemplate;
 	
 	@GetMapping("/{userId}")
 	public User getUser(@PathVariable("userId") Long userId) {
-		return this.userService.getUser(userId);
 		
+		User user = this.userService.getUser(userId);
+		
+		//http://localhost:9002/contact/user/1311
+		List contacts = this.restTemplate.getForObject("http://localhost:9002/contact/user/" + userId, List.class);
+		user.setContacts(contacts);
+		return user;
 	}
 
 }
